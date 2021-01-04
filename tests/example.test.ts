6e6e266ad2d4b4d9c87da38309cbd1183d98dcfe
Alex Renoki <rennokki@gmail.com>
2021-01-04 10:49:37 +0200

    wip test

---
+++ tests/example.test.ts
@@ -0,0 +1,9 @@
+describe('test', () => {
+    beforeEach(() => {
+        console.log('Test is running...');
+    });
+
+    test('some stuff', () => {
+        expect(true).toBe(true);
+    });
+});
